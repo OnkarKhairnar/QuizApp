5e54fbbb5f39d4dc6168df69331f0f01617ffffe
Onkar Khairnar <99865948+OnkarKhairnar@users.noreply.github.com>
2026-01-09 18:27:59 +0530

    Backend added

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { register } = require('../controllers/authController');
-
-const router = express.Router();
-
-// Registration route
-router.post('/register', register);
-
-module.exports = router;
